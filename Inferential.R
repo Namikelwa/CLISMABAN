380151d4cd9de0982e2046355271c9974856b43b
Dorcus Namikelwa <85280529+Namikelwa@users.noreply.github.com>
2022-05-31 10:50:38 +0300

    Update Inferential.R

--- Inferential.R
+++ Inferential.R
@@ -33,6 +33,72 @@ sub1 <- subset(my_data, Gender=="Female" & Consumption != "Never")
 #subset out id of 8 and 9
 sub1 <- subset(my_data, !ID%in% c(8,9))
 
+
+#subsetting data by attribute and visualizing the statistics
+
+##APPEARANCE
+
+boiledA <- select (my_data, starts_with("BA"))
+boiledA <- select (my_data, Preference, starts_with("BA"))
+BarChart(data=boiledA)
+
+mashedA <- select (my_data, starts_with("MA"))
+BarChart(data=mashedA)
+
+stewdA<- select (my_data, starts_with("SA"))
+BarChart(data=stewdA)
+
+FriedA<- select (my_data, starts_with("FA"))
+BarChart(data=FriedA)
+
+##SMELL
+
+#boiledS<- select (my_data, starts_with("BS"))
+#BarChart(data=boiledS)
+
+mashedS<- select(my_data, starts_with("MS"))
+BarChart(data=mashedS)
+
+stewedS<- select(my_data, starts_with("SS"))
+BarChart(data=stewedS)
+
+friedS<- select(my_data, starts_with("FS"))
+BarChart(data=friedS)
+
+##TASTE
+mashedT<- select(my_data, starts_with("MT"))
+BarChart(data=mashedT)
+
+boiledT<- select (my_data, starts_with("BT"))
+BarChart(data=boiledT)
+
+stewedT<- select(my_data, starts_with("ST"))
+BarChart(data=stewedT)
+
+friedT<- select(my_data, starts_with("FT"))
+BarChart(data=friedT)
+
+
+#statics for every variety
+Line1<- select(my_data, ends_with("1"))
+BarChart(data=Line1)
+
+Line2<- select(my_data, ends_with("2"))
+BarChart(data=Line2)
+
+Line3<- select(my_data, ends_with("3"))
+BarChart(data=Line3)
+
+Line4<- select(my_data, ends_with("4"))
+BarChart(data=Line4)
+
+Line5<- select(my_data, ends_with("5"))
+BarChart(data=Line5)
+
+Line6<- select(my_data, ends_with("6"))
+BarChart(data=Line6)
+
+
 #The above is same as running the two codes below
 sub1 <- subset(my_data, ID != 8)
 sub1 <- subset(my_data, ID != 9)
